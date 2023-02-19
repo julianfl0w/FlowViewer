cd61f7222e3b9792f486a4854aa87f115564e418
Julian Loiacono <jcloiacon@gmail.com>
2023-02-19 09:27:52 -0500

    includes dependencies

---
+++ silk-3.19.2/src/libsilk/rwpack.h
@@ -0,0 +1,33 @@
+/*
+** Copyright (C) 2001-2022 by Carnegie Mellon University.
+**
+** @OPENSOURCE_LICENSE_START@
+** See license information in ../../LICENSE.txt
+** @OPENSOURCE_LICENSE_END@
+*/
+#ifndef _RWPACK_H
+#define _RWPACK_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <silk/silk.h>
+
+RCSIDENTVAR(rcsID_RWPACK_H, "$SiLK: rwpack.h ba4a308de770 2022-03-02 22:29:28Z mthomas $");
+
+#include <silk/rwrec.h>
+#include <silk/skstream.h>
+#include <silk/rwascii.h>
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _RWPACK_H */
+
+/*
+** Local Variables:
+** mode:c
+** indent-tabs-mode:nil
+** c-basic-offset:4
+** End:
+*/
